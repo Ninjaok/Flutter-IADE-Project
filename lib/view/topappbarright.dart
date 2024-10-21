27840167bcaa84f57c768a132f20a0b9b54678b1
Luan Ribeiro <20230692@iade.pt>
2024-10-21 18:05:46 +0100

    top bar with button

--- lib/view/topappbarright.dart
+++ lib/view/topappbarright.dart
@@ -8,7 +8,51 @@ class TopAppBarRight extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Column(
-      children: [SizedBox(width: 300, child: Text("A"))],
+      mainAxisAlignment: MainAxisAlignment.end,
+      children: [
+        Center(
+            child: Container(
+                width: 450,
+                color: Colors.red,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ButtonTabBarAction(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ButtonTabBarAction(),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(4.0),
+                      child: ButtonTabBarAction(),
+                    ),
+                  ],
+                )))
+      ],
+    );
+  }
+}
+
+class ButtonTabBarAction extends StatelessWidget {
+  const ButtonTabBarAction({
+    super.key,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        ElevatedButton(
+          onPressed: () {},
+          // style: ButtonStyle(elevation: MaterialStateProperty(12.0 )),
+          style: ElevatedButton.styleFrom(
+              elevation: 12.0, textStyle: const TextStyle(color: Colors.white)),
+          child: const Text('__Button__'),
+        ),
+      ],
     );
   }
 }
